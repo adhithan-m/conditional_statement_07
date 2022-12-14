5d00b42275049fc3327b9a7eb2020bd4146c11d1
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-14 16:53:57 +0530

    young and old.

---
+++ js/old.js
@@ -0,0 +1,49 @@
+//3-Take input of age of 3 people by user and determine oldest and youngest among them.
+
+var x = parseInt(prompt("enter 1st people age"));
+var y = parseInt(prompt("enter 2nd people age"));
+var z = parseInt(prompt("enter 3rd people age"));
+
+
+if (x>y && x>z)
+{
+	alert ("x is oldest");
+	
+	if (y<z)
+	{
+		alert ("y is youngest");
+	}
+	else
+	{
+		alert ("z is youngest");
+	}
+	
+}
+	
+if (y>x && y>z)
+{
+   alert ("y is oldest");
+	
+   if (x<z)
+   {
+        alert ("x is youngest");
+   }
+   else
+   {
+        alert ("z is youngest");
+   }	
+	
+}			
+else (z>x && z>y)
+{
+   alert ("z is oldest");
+	
+   if (x<y)
+   {
+        alert ("x is youngest");
+   }
+   else
+   {
+        alert ("y is youngest");
+   }	
+}
